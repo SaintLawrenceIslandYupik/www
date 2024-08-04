372420b05168610ee5b54bcb7d6c5aeb04ca6917
Lane Schwartz <dowobeha@gmail.com>
2024-08-03 17:44:01 -1100

    Add buttons

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -2,28 +2,157 @@ function myFunction(element, color) {
   element.style.color = color;
 }
 
-function myHide() {
-  
-  for (const tag of ["tei-pb", "tei-graphic", "tei-p", "tei-list"]) {
+function startBook() {
+  for (const tag of ["tei-titlePage", "tei-div"]) {
     //const tag="tei-pb";
     const allParas = document.getElementsByTagName(tag);
     
-    const num = allParas.length;
+    for (const div of allParas) {
+      //if (div.style.display === "") {
+        div.style.display = "none";
+      //}
+    }
+    
+  }
+  
+  for (const tag of ["tei-titlePage"]) {
+    //const tag="tei-pb";
+    const allParas = document.getElementsByTagName(tag);
+    
+    for (const div of allParas) {
+      if (div.style.display === "none") {
+        div.style.display = "block";
+      }
+    }
+    
+  }
+  
+  
+}
+
+function prevPage() {
+  
+  let foundCurrentPage = false;
+  //alert("prev");
+  for (const tag of ["tei-div"]) {
+    //const tag="tei-pb";
+    const allDivs = document.getElementsByTagName(tag);
+    
+    const num = allDivs.length;
     //alert(`There are ${num} ${tag} tags in this document`);
     
-    //allParas.forEach(function(element) {
-    for (const x of allParas) {
-      //alert(`In for-each`);
-      //element.style.visibility = 'hidden';
-      if (x.nodeName === "BUTTON") {
-        alert(`Button`);
-      } else {
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
+    for (let i=num-1; i >= 0; i-=1) {
+      
+      if (allDivs[i].style.display === "block") {
+        //alert(`Hiding ${x.innerText}`);
+        foundCurrentPage = true;
+        allDivs[i].style.display = "none";
+        
+      } else if (foundCurrentPage) {
+        
+        if (allDivs[i].style.display === "none") {
+          allDivs[i].style.display = "block";
+          //alert(`Showing ${x.innerText}`);
+          return;
         }
       }
+      
+    }
+  }
+    
+    for (const tag of ["tei-titlePage"]) {
+      //const tag="tei-pb";
+      const allParas = document.getElementsByTagName(tag);
+      
+      for (const div of allParas) {
+        if (div.style.display === "none") {
+          div.style.display = "block";
+        }
+      }
+      
+    }
+  
+}
+
+function nextPage() {
+  let foundCurrentPage = false;
+  
+  const allTitle = document.getElementsByTagName("tei-titlePage");
+  for (let i=0; i < allTitle.length; i+=1) {
+    if (allTitle[i].style.display === "block") {
+        //alert(`Hiding ${x.innerText}`);
+        foundCurrentPage = true;
+        allTitle[i].style.display = "none";
+      
+      } else if (foundCurrentPage) {
+        
+        if (allTitle[i].style.display === "none") {
+          allTitle[i].style.display = "block";
+          /*
+          x.addEventListener('click', (event) => {   // Do something when a swipe event occurs });
+            myHide();
+          });
+          x.addEventListener('dblclick', (event) => {   // Do something when a swipe event occurs });
+            alert("double click");
+          });
+           */
+          //alert(`Showing ${x.innerText}`);
+          return;
+        }
+      }
+    //}
+  }
+  
+  
+  for (const tag of ["tei-div"]) {
+    //const tag="tei-pb";
+    const allDivs = document.getElementsByTagName(tag);
+    
+    const num = allDivs.length;
+    //alert(`There are ${num} ${tag} tags in this document`);
+
+    
+    //allParas.forEach(function(element) {
+    //const num = allDivs.length;
+    //alert(`There are ${num} ${tag} tags in this document`);
+    
+    for (let i=0; i < num; i+=1) {
+      
+      //let x = allDivs[i];
+      //alert(i);
+      //element.style.visibility = 'hidden';
+      //if (x.nodeName === "BUTTON") {
+        //`alert(x.innerText);
+      //} else {
+      
+      
+      //alert(`'${x.style.display}'`);
+      //break;
+      
+      if (allDivs[i].style.display === "block") {
+          //alert(`Hiding ${x.innerText}`);
+          foundCurrentPage = true;
+        if (i+1 < num) {
+          allDivs[i].style.display = "none";
+        }
+        
+        } else if (foundCurrentPage) {
+          
+          if (allDivs[i].style.display === "none") {
+            allDivs[i].style.display = "block";
+            /*
+            x.addEventListener('click', (event) => {   // Do something when a swipe event occurs });
+              myHide();
+            });
+            x.addEventListener('dblclick', (event) => {   // Do something when a swipe event occurs });
+              alert("double click");
+            });
+             */
+            //alert(`Showing ${x.innerText}`);
+            return;
+          }
+        }
+      //}
     }
   }
   //});
--- assets/js/loadbook.js
+++ assets/js/loadbook.js
@@ -9,6 +9,24 @@ let behaviors = {
       }
       return result;
     },
+    "title": [
+      ["tei-titlestmt>tei-title", function(elt) {
+        const existingTitleTags = document.getElementsByTagName("title");
+        if (existingTitleTags.length < 1) {
+          let title = document.createElement("title");
+          title.innerHTML = elt.innerText;
+          document.querySelector("head").appendChild(title);
+        } /*else {
+          existingTitleTags[0].innerHTML += ", " + elt.innerText;
+        }*/
+      }]
+    ],
+    //"titlePage": ["<div class=\"titlePage\">", "</div>"],
+    "titlePart": function(e) {
+      let h1 = document.createElement("h1");
+      h1.innerHTML = e.innerHTML;
+      return h1;
+    },
     "lb": ["<br>"],
     /* Insert a <p> with the content of the <pb>'s @n attribute inside it
        Add a line above with CSS */
@@ -21,3 +39,17 @@ c.getHTML5('assets/tei/demo.xml', function(data){
   document.getElementsByTagName('body')[0].appendChild(data);
 });
 
+
+/*
+  for (const tagName of ["tei-div"]) {
+    
+    const elements = document.getElementsByTagName(tag);
+    alert(`finally`);
+    for (const element of elements) {
+      if (x.style.display === "") {
+        alert("Set to none");
+        x.style.display = "none";
+      }
+    }
+  }
+  */
